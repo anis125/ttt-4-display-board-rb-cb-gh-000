ae2bb9e6dead2b9913970f4f3b80d30db21f22c3
Anis Ghliss <anisghliss20@gmail.com>
2017-11-18 17:25:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(tab)
-puts(" #{tab[0]} | #{tab[0]}  | #{tab[0]}  ")
+puts(" #{tab[0]} | #{tab[0]} | #{tab[0]}  ")
 puts("-----------")
-puts(" #{tab[3]} | #{tab[4]}  | #{tab[5]}  ")
+puts(" #{tab[3]} | #{tab[4]} | #{tab[5]}  ")
 puts("-----------")
-puts(" #{tab[6]} | #{tab[7]}  | #{tab[8]}  ")
+puts(" #{tab[6]} | #{tab[7]} | #{tab[8]}  ")
 end
 display_board([" "," "," "," "," "," "," "," "," "])
